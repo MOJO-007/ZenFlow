cad64af3efff51cb7b9475324f01e471d1cc8062
MOJO <abhisatish75@gmail.com>
2024-02-21 10:51:25 +0530

    Few Screens UI Done

---
+++ app/src/main/java/com/example/zenflow/ui/activity/MainContainerActivity.kt
@@ -0,0 +1,126 @@
+package com.example.zenflow.ui.activity
+
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+//import com.example.weis.R
+//import com.example.weis.databinding.ActivityMainContainerBinding
+import com.example.zenflow.adapters.ViewPagerAdapter
+//import com.example.weis.ui.fragment.HomeFragment
+//import com.example.weis.ui.fragment.FocusFragment
+//import com.example.weis.ui.fragment.GoalsFragment
+import com.example.zenflow.R
+import com.example.zenflow.databinding.ActivityMainContainerBinding
+import com.example.zenflow.ui.fragment.GuidedFragment
+import com.example.zenflow.ui.fragment.RecommendedFragment
+import com.example.zenflow.ui.fragment.SleepyFragment
+import com.example.zenflow.ui.fragment.UnguidedFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainContainerActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainContainerBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainContainerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.primary)
+
+        val viewPagerAdapter = ViewPagerAdapter(this)
+        viewPagerAdapter.addFragment(
+            RecommendedFragment(),
+            getString(R.string.recommended),
+            getColorIcon(R.drawable.ic_recommended, R.color.grey2),
+            getColorIcon(R.drawable.ic_recommended, R.color.primary)
+        )
+        viewPagerAdapter.addFragment(
+            GuidedFragment(),
+            getString(R.string.guided),
+            getColorIcon(R.drawable.ic_guided, R.color.grey2),
+            getColorIcon(R.drawable.ic_guided, R.color.primary)
+        )
+        viewPagerAdapter.addFragment(
+            UnguidedFragment(),
+            getString(R.string.unguided),
+            getColorIcon(R.drawable.ic_unguided, R.color.grey2),
+            getColorIcon(R.drawable.ic_unguided, R.color.primary)
+        )
+        viewPagerAdapter.addFragment(
+            SleepyFragment(),
+            getString(R.string.sleepy),
+            getColorIcon(R.drawable.ic_sleepy, R.color.grey2),
+            getColorIcon(R.drawable.ic_sleepy, R.color.primary)
+        )
+
+        binding.viewPagerMain.adapter = viewPagerAdapter
+        binding.viewPagerMain.setCurrentItem(1, true)
+
+        TabLayoutMediator(binding.cardTabLay, binding.viewPagerMain) { tab, position ->
+            val customTabView = viewPagerAdapter.getTabView(position)
+            tab.customView = customTabView
+            if(position == 1){
+                tab.let {
+                    val customTabView = it.customView
+                    val tabIconImageView = customTabView?.findViewById<ImageView>(R.id.tabIcon)
+                    val tabNameTextView = customTabView?.findViewById<TextView>(R.id.tabName)
+
+                    tabIconImageView?.setColorFilter(ContextCompat.getColor(this@MainContainerActivity, R.color.primary))
+                    tabNameTextView?.setTextColor(ContextCompat.getColor(this@MainContainerActivity, R.color.primary))
+                }
+            }
+        }.attach()
+        binding.cardTabLay.getTabAt(1)?.select()
+
+        binding.cardTabLay.addOnTabSelectedListener(object : OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.let {
+                    val customTabView = it.customView
+                    val tabIconImageView = customTabView?.findViewById<ImageView>(R.id.tabIcon)
+                    val tabNameTextView = customTabView?.findViewById<TextView>(R.id.tabName)
+
+                    tabIconImageView?.setColorFilter(ContextCompat.getColor(this@MainContainerActivity, R.color.primary))
+                    tabNameTextView?.setTextColor(ContextCompat.getColor(this@MainContainerActivity, R.color.primary))
+                }
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                tab?.let {
+                    val customTabView = it.customView
+                    val tabIconImageView = customTabView?.findViewById<ImageView>(R.id.tabIcon)
+                    val tabNameTextView = customTabView?.findViewById<TextView>(R.id.tabName)
+
+                    tabIconImageView?.setColorFilter(ContextCompat.getColor(this@MainContainerActivity, R.color.grey2))
+                    tabNameTextView?.setTextColor(ContextCompat.getColor(this@MainContainerActivity, R.color.grey2))
+                }
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                tab?.let {
+                    val customTabView = it.customView
+                    val tabIconImageView = customTabView?.findViewById<ImageView>(R.id.tabIcon)
+                    val tabNameTextView = customTabView?.findViewById<TextView>(R.id.tabName)
+
+                    tabIconImageView?.setColorFilter(ContextCompat.getColor(this@MainContainerActivity, R.color.primary))
+                    tabNameTextView?.setTextColor(ContextCompat.getColor(this@MainContainerActivity, R.color.primary))
+                }
+            }
+
+        })
+    }
+
+    private fun getColorIcon(icon: Int, color: Int): Drawable? {
+        val drawable = ContextCompat.getDrawable(this, icon)?.mutate()
+        drawable?.colorFilter = PorterDuffColorFilter(ContextCompat.getColor(this, color), PorterDuff.Mode.SRC_IN)
+        return drawable
+    }
+}
--- app/src/main/java/com/example/zenflow/ui/activity/TypeSelectionActivity.kt
+++ app/src/main/java/com/example/zenflow/ui/activity/TypeSelectionActivity.kt
@@ -1,11 +1,12 @@
-package com.example.zenflow
+package com.example.zenflow.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.zenflow.R
 
-class MainActivity : AppCompatActivity() {
+class TypeSelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_type_selection)
     }
 }
