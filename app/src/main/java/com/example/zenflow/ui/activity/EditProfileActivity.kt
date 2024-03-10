fbc8cafc972ef08e2038c8de439ad45e4d9b78a9
MOJO <abhisatish75@gmail.com>
2024-03-10 13:38:03 +0530

    Username and mobile number updates working, added a swipe refresh view for the UserDetailsFragment.

--- app/src/main/java/com/example/zenflow/ui/activity/EditProfileActivity.kt
+++ app/src/main/java/com/example/zenflow/ui/activity/EditProfileActivity.kt
@@ -1,15 +1,26 @@
 package com.example.zenflow.ui.activity
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.zenflow.R
 import com.example.zenflow.databinding.ActivityEditProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditProfileBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var dbref: CollectionReference
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        dbref=FirebaseFirestore.getInstance().collection("user")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
 
@@ -17,10 +28,27 @@ class EditProfileActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val userName = intent.getStringExtra("userName")
+        val id = intent.getStringExtra("id").toString()
         val userMobileNumber = intent.getStringExtra("userMobileNumber")
 
         binding.editTextUserName.setText(userName)
         binding.editTextMobileNumber.setText(userMobileNumber)
+
+        binding.btnSaveChanges.setOnClickListener {
+            val updates = hashMapOf<String, Any>()
+            updates["userName"] = binding.editTextUserName.text.toString()
+            updates["userMobileNumber"] = binding.editTextMobileNumber.text.toString()
+            val docRef = dbref.document(id)
+            docRef.update(updates)
+                .addOnSuccessListener {
+                    Toast.makeText(this , "UserProfile Updated", Toast.LENGTH_LONG).show()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(this , "Update Failed", Toast.LENGTH_LONG).show()
+                }
+            this.finish()
+        }
     }
+
 
 }
--- app/src/main/java/com/example/zenflow/ui/fragment/UserDetailsFragment.kt
+++ app/src/main/java/com/example/zenflow/ui/fragment/UserDetailsFragment.kt
@@ -15,6 +15,7 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.net.toUri
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.bumptech.glide.Glide
 import com.example.zenflow.R
 import com.example.zenflow.databinding.FragmentRegisterBinding
@@ -33,7 +34,9 @@ import com.google.firebase.storage.storage
 
 class UserDetailsFragment : Fragment() {
 
-    private var imageUri:Uri?=null
+    private val rq = 1
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    private var imageUri: Uri? = null
     private lateinit var auth: FirebaseAuth
 
     private lateinit var dbref: CollectionReference
@@ -44,24 +47,24 @@ class UserDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         dbref = FirebaseFirestore.getInstance().collection("user")
-        binding= FragmentUserDetailsBinding.inflate(inflater,container,false)
+        binding = FragmentUserDetailsBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val userName = arguments?.getString("userName")
-        val id:String? = arguments?.getString("id")
-        var imageUri:Uri? = arguments?.getString("ImageUri")?.toUri()
+        var userName = arguments?.getString("userName")
+        val id: String? = arguments?.getString("id")
+        var imageUri: Uri? = arguments?.getString("ImageUri")?.toUri()
         val email = arguments?.getString("userEmail")
         val userMobileNumber = arguments?.getString("userMobileNumber")
 //        Toast.makeText(requireContext(), "$imageUri", Toast.LENGTH_LONG).show()
-        if (imageUri == null){
+        if (imageUri == null) {
             Glide.with(this)
                 .load(R.drawable.ic_add_image)
                 .error((R.drawable.ic_add_image))
                 .into(binding.userImage)
-        }else{
+        } else {
             Glide.with(this)
                 .load(imageUri)
                 .error(R.drawable.ic_add_image)
@@ -73,31 +76,30 @@ class UserDetailsFragment : Fragment() {
 
 
         super.onViewCreated(view, savedInstanceState)
-        auth=FirebaseAuth.getInstance()
+        auth = FirebaseAuth.getInstance()
 
 
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            val storage = Firebase.storage
+            val storageRef = storage.reference.child("pictures/$id")
+            imageUri = it
+            val fileRef = storageRef.child("pictures")
+            val uploadTask = fileRef.putFile(Uri.parse(imageUri.toString()))
 
-         val resultLauncher = registerForActivityResult(ActivityResultContracts.GetContent()){
-             val storage = Firebase.storage
-             val storageRef = storage.reference.child("pictures/$id")
-             imageUri = it
-             val fileRef = storageRef.child("pictures")
-             val uploadTask = fileRef.putFile(Uri.parse(imageUri.toString()))
-
-             uploadTask.addOnCompleteListener { _ ->
-                 fileRef.downloadUrl.addOnSuccessListener { uri ->
-                     binding.userImage.setImageURI(it)
-                     val imUri = uri.toString()
-                     val userDocRef = id?.let { it1 -> dbref.document(it1) }
-                     userDocRef?.update("imageUri", imUri)
-                         ?.addOnSuccessListener {
-                             Log.e("Done", "Donee")
-                         }
-                 }
-             }
+            uploadTask.addOnCompleteListener { _ ->
+                fileRef.downloadUrl.addOnSuccessListener { uri ->
+                    binding.userImage.setImageURI(it)
+                    val imUri = uri.toString()
+                    val userDocRef = id?.let { it1 -> dbref.document(it1) }
+                    userDocRef?.update("imageUri", imUri)
+                        ?.addOnSuccessListener {
+                            Log.e("Done", "Donee")
+                        }
+                }
+            }
             binding.userImage.setImageURI(it)
         }
-        binding.userImage.setOnClickListener{
+        binding.userImage.setOnClickListener {
             resultLauncher.launch("image/*")
         }
 
@@ -110,14 +112,25 @@ class UserDetailsFragment : Fragment() {
 
         binding.btnEditProfile.setOnClickListener {
             val intent = Intent(requireContext(), EditProfileActivity::class.java)
-            intent.putExtra("id",id)
-            intent.putExtra("userEmail",email)
-            intent.putExtra("userName",userName)
-            intent.putExtra("imageUri",imageUri)
-            intent.putExtra("userMobileNumber",userMobileNumber)
+            intent.putExtra("id", id)
+            intent.putExtra("userEmail", email)
+            intent.putExtra("userName", userName)
+            intent.putExtra("imageUri", imageUri)
+            intent.putExtra("userMobileNumber", userMobileNumber)
+
             startActivity(intent)
-            requireActivity().finish()
+//            requireActivity().finish()
+        }
+
+        swipeRefreshLayout = binding.swipeRefreshLayout
+        swipeRefreshLayout.setOnRefreshListener {
+            val q = dbref.whereEqualTo("userEmail", email)
+            q.get().addOnCompleteListener {
+                val ds = it.result?.getDocuments()?.get(0)
+                userName = ds?.getString("userName").toString()
+                binding.textViewUserNAme.setText(userName)
+                swipeRefreshLayout.isRefreshing = false
+            }
         }
     }
-
 }
